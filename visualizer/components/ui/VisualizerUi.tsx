55e32c5f492607aa5826b347b8bb1b1f7b8b03dd
unknown <singhrathoreayush824@gmail.com>
2025-08-27 17:28:04 +0530

    VisualizerUi

---
+++ visualizer/components/ui/VisualizerUi.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Search as SearchIcon, X } from "lucide-react";
+
+const roles = ["Asia","North America","South America","Europe","Africa","Antarctica","Australia"];
+const daysOptions = [1, 3, 10, 20, 30];
