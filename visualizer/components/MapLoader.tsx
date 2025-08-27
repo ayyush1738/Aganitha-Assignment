13dec318952f92047945827136a93a895f6c8234
unknown <singhrathoreayush824@gmail.com>
2025-08-27 18:15:53 +0530

    Map

--- visualizer/components/MapLoader.tsx
+++ visualizer/components/MapLoader.tsx
@@ -1,25 +1,73 @@
-"use client"; 
+"use client";
 
-import dynamic from 'next/dynamic';
-import { useMemo } from 'react';
-import { EarthquakeData } from "@/types/earthquake";
+import { MapContainer, TileLayer, CircleMarker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
+// Define the types for the earthquake data based on the GeoJSON structure
+interface EarthquakeFeature {
+  type: "Feature";
+  properties: {
+    mag: number;
+    place: string;
+    time: number;
+    title: string;
+  };
+  geometry: {
+    type: "Point";
+    coordinates: [number, number, number]; // [longitude, latitude, depth]
+  };
+  id: string;
+}
 
-interface MapLoaderProps {
+interface EarthquakeData {
+  type: "FeatureCollection";
+  features: EarthquakeFeature[];
+}
+
+interface EarthquakeMapProps {
   earthquakes: EarthquakeData;
 }
 
-const MapLoader: React.FC<MapLoaderProps> = ({ earthquakes }) => {
+const EarthquakeMap: React.FC<EarthquakeMapProps> = ({ earthquakes }) => {
+  return (
+    <MapContainer
+      center={[20, 0]}
+      zoom={2}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      
+      {earthquakes.features.map((earthquake, index) => {
+  const { coordinates } = earthquake.geometry;
+  const { title, mag, time } = earthquake.properties;
+  const date = new Date(time);
 
-    const Map = useMemo(() => dynamic(
-    () => import('@/components/EarthquakeMap'), 
-    {
-      loading: () => <p>A map is loading...</p>,
-      ssr: false 
-    }
-  ), []); 
+  // Leaflet expects [latitude, longitude]
+  const position: [number, number] = [coordinates[1], coordinates[0]];
 
-  return <Map earthquakes={earthquakes} />;
+  return (
+    <CircleMarker
+      key={`${earthquake.id}_${index}`} // ✅ unique key
+      center={position}
+      radius={mag * 4} // marker size based on magnitude
+      color="red"
+      fillColor="red"
+      fillOpacity={0.4}
+      stroke={false}
+    >
+      <Popup>
+        <b>{title}</b>
+        <br />
+        Magnitude: {mag}
+        <br />
+        Time: {date.toLocaleString()}
+      </Popup>
+    </CircleMarker>
+  );
+})}
+
+    </MapContainer>
+  );
 };
 
-export default MapLoader;
+export default EarthquakeMap;
